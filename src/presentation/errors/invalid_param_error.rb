a6c849dd4938cdd19ef26ed37643df31b11f1de1
silva4dev <web4dev.lucas@outlook.com>
2023-12-14 17:40:12 -0300

    feat: add invalid param error

---
+++ src/presentation/errors/invalid_param_error.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Presentation
+  module Errors
+    class InvalidParamError < StandardError
+      def initialize(param_name)
+        super("Invalid param: #{param_name}")
+        @name = "InvalidParamError"
+      end
+    end
+  end
+end
