7f9fc55c215a664f93611f5a3f43579a8d5533de
silva4dev <web4dev.lucas@outlook.com>
2023-12-14 17:41:52 -0300

    chore: move to tests folder

---
+++ tests/unit/presentation/controllers/signup_controller_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative "../../../../src/presentation/controllers/signup/signup_controller"
+require_relative "../../../../src/presentation/errors/missing_param_error"
+
+describe Presentation::Controllers::Signup::SignupController, type: :unit do
+  it "Should return 400 if required fields are not passed in the HTTP request body" do
+    http_request = {
+      body: {
+        read: {}
+      }
+    }
+    sut = Presentation::Controllers::Signup::SignupController.new
+    http_response = sut.handle(http_request)
+
+    expect(http_response[:status_code]).to eq(400)
+    expect(http_response[:body]).to be_instance_of(Presentation::Errors::MissingParamError)
+  end
+end
