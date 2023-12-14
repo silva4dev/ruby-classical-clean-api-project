753808f800e5c277fadb7ad10416fa7f4d8263b9
silva4dev <web4dev.lucas@outlook.com>
2023-12-14 19:27:29 -0300

    chore: ensure add sinatra reloader

--- src/main/server.rb
+++ src/main/server.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "sinatra/reloader"
 require_relative "./config/routes"
 
 module Main
@@ -7,6 +8,10 @@ module Main
     configure do
       set :bind, "0.0.0.0"
       set :port, 3333
+    end
+
+    configure :development do
+      register Sinatra::Reloader
     end
   end
 end
