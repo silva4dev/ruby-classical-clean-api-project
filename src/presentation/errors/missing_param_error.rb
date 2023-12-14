4834ae3a36ec9ea3cd869e6df4ad9a61886d9d35
silva4dev <web4dev.lucas@outlook.com>
2023-12-14 17:40:21 -0300

    feat: add missing param error

---
+++ src/presentation/errors/missing_param_error.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Presentation
+  module Errors
+    class MissingParamError < StandardError
+      def initialize(param_name)
+        super("Missing param: #{param_name}")
+        @name = "MissingParamError"
+      end
+    end
+  end
+end
