b98d5c26776817a0f6e0df6067dcc2dcf8c3b729
silva4dev <web4dev.lucas@outlook.com>
2023-12-16 19:44:48 -0300

    test: ensure calls SignupController to return 400 if the password doesn't match the password confirmation

--- tests/unit/presentation/controllers/signup_controller_spec.rb
+++ tests/unit/presentation/controllers/signup_controller_spec.rb
@@ -2,19 +2,33 @@
 
 require_relative "../../../../src/presentation/controllers/signup/signup_controller"
 require_relative "../../../../src/presentation/errors/missing_param_error"
+require_relative "../../../../src/presentation/errors/invalid_param_error"
 
 describe Presentation::Controllers::Signup::SignupController, type: :unit do
   it "Should return 400 if required fields are not passed in the HTTP request body" do
     http_request = {
-      body: {
-        read: {}
-      }
+      body: {},
     }
-
     sut = Presentation::Controllers::Signup::SignupController.new
     http_response = sut.handle(http_request)
 
     expect(http_response[:status_code]).to eq(400)
     expect(http_response[:body]).to be_instance_of(Presentation::Errors::MissingParamError)
+  end
+
+  it "Should return 400 if the password doesn't match the password confirmation" do
+    http_request = {
+      body: {
+        name: "any_name",
+        email: "any_email",
+        password: "any_password",
+        password_confirmation: "any_password_confirmation",
+      },
+    }
+    sut = Presentation::Controllers::Signup::SignupController.new
+    http_response = sut.handle(http_request)
+
+    expect(http_response[:status_code]).to eq(400)
+    expect(http_response[:body]).to eq(Presentation::Errors::InvalidParamError.new(:password_confirmation))
   end
 end
