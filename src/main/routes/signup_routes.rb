34f3814af69c1f3e3973424b6c625a7b3b110dfb
silva4dev <web4dev.lucas@outlook.com>
2023-12-16 21:01:29 -0300

    chore: ensure calls SignupRoutes with SignupController using adapt route

--- src/main/routes/signup_routes.rb
+++ src/main/routes/signup_routes.rb
@@ -2,12 +2,15 @@
 
 require_relative "../../presentation/controllers/signup/signup_controller"
 require_relative "../config/middlewares"
+require_relative "../adapters/sinatra_route_adapter"
 
 module Main
   module Routes
     class SignupRoutes < Config::Middlewares
       post "/signup" do
-        return Presentation::Controllers::Signup::SignupController.new.handle(to_hash(request)).to_json
+        return adapt_route(Presentation::Controllers::Signup::SignupController.new)
+          .call(request, response)
+          .body
       end
     end
   end
