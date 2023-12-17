417c937de5e2ca401c5323cc6ea5d4aff864056d
silva4dev <web4dev.lucas@outlook.com>
2023-12-16 21:02:54 -0300

    chore: ensure use body parser middleware

--- src/main/config/middlewares.rb
+++ src/main/config/middlewares.rb
@@ -1,23 +1,12 @@
 # frozen_string_literal: true
 
 require "sinatra/base"
+require_relative "../middlewares/body_parser"
 
 module Main
   module Config
     class Middlewares < Sinatra::Base
-      before do
-        request.body.rewind
-        content_type "application/json"
-      end
-
-      def to_hash(request)
-        {
-          query_params: request.params,
-          body: (request.body && JSON.parse(request.body.read, symbolize_names: true)) || {}
-        }
-      rescue JSON::ParserError
-        {}
-      end
+      use Main::Middlewares::BodyParserMiddleware
     end
   end
 end
