78a87ddae0bcff6cb046594c07a816510fbbe243
silva4dev <web4dev.lucas@outlook.com>
2023-12-20 16:49:05 -0300

    chore: ensure use cors middleware

--- src/main/config/sinatra/middlewares.rb
+++ src/main/config/sinatra/middlewares.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
-require_relative "../../middlewares/sinatra/body_parser"
+require_relative "../../middlewares/sinatra/content_type"
+require_relative "../../middlewares/sinatra/cors"
 require "sinatra/base"
 
 module Main
   module Config
     class Middlewares < Sinatra::Base
-      use Main::Middlewares::Sinatra::BodyParserMiddleware
+      use Main::Middlewares::Sinatra::ContentTypeMiddleware
+      use Main::Middlewares::Sinatra::CorsMiddleware
     end
   end
 end
