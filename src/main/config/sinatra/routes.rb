45f87dc69c831043edc0a69f406aa51b22722553
silva4dev <web4dev.lucas@outlook.com>
2023-12-20 16:38:57 -0300

    chore: ensure rename imports

--- src/main/config/sinatra/routes.rb
+++ src/main/config/sinatra/routes.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
+require_relative "../../routes/sinatra/signup_routes"
 require "sinatra/base"
-require_relative "../routes/signup_routes"
 
 module Main
   module Config
     class Routes < Sinatra::Base
-      use Main::Routes::SignupRoutes
+      use Main::Routes::Sinatra::SignupRoutes
     end
   end
 end
