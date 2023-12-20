c3b44b92e63fae2db2c2d90d8af99b93f94f1a6c
silva4dev <web4dev.lucas@outlook.com>
2023-12-20 16:39:38 -0300

    refactor: ensure move to sinatra folder

---
+++ src/main/routes/sinatra/signup_routes.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative "../../../presentation/controllers/signup/signup_controller"
+require_relative "../../config/sinatra/middlewares"
+require_relative "../../adapters/sinatra/route_adapter"
+require_relative "../../factories/sinatra/signup"
+
+module Main
+  module Routes
+    module Sinatra
+      class SignupRoutes < Main::Config::Middlewares
+        post "/signup" do
+          return Adapters::Sinatra::SinatraRouteAdapter.adapt(Factories::Sinatra::SignupFactory.create())
+            .call(request, response)
+            .body
+        end
+      end
+    end
+  end
+end
