7fcd3203d797555927a6b8cde267edb5262b8e74
silva4dev <web4dev.lucas@outlook.com>
2023-12-20 16:39:07 -0300

    feat: add signup factory

---
+++ src/main/factories/sinatra/signup.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative "../../../utils/email_validator_adapter"
+require_relative "../../../presentation/controllers/signup/signup_controller"
+
+module Main
+  module Factories
+    module Sinatra
+      class SignupFactory
+        def self.create
+          email_validator_adapter = Utils::EmailValidatorAdapter.new
+          return Presentation::Controllers::Signup::SignupController.new(email_validator_adapter)
+        end
+      end
+    end
+  end
+end
