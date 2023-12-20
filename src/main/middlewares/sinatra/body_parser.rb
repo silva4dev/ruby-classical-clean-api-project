f34161e765e623b75c39b9e0a6989d94ef42a991
silva4dev <web4dev.lucas@outlook.com>
2023-12-20 16:39:25 -0300

    refactor: ensure move to sinatra folder

---
+++ src/main/middlewares/sinatra/body_parser.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Main
+  module Middlewares
+    module Sinatra
+      class BodyParserMiddleware
+        def initialize(app)
+          @app = app
+        end
+
+        def call(env)
+          status, headers, body = @app.call(env)
+          headers["Content-Type"] = "application/json"
+          [status, headers, body]
+        end
+      end
+    end
+  end
+end
