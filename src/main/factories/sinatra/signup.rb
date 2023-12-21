1991e8832c45b6d9521059775e9e499e5330165e
silva4dev <web4dev.lucas@outlook.com>
2023-12-21 01:30:33 -0300

    refactor: ensure move to email validator folder

--- src/main/factories/sinatra/signup.rb
+++ src/main/factories/sinatra/signup.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "../../../utils/email_validator_adapter"
+require_relative "../../../utils/email_validator/email_validator_adapter"
 require_relative "../../../presentation/controllers/signup/signup_controller"
 
 module Main
@@ -8,7 +8,7 @@ module Main
     module Sinatra
       class SignupFactory
         def self.create
-          email_validator_adapter = Utils::EmailValidatorAdapter.new
+          email_validator_adapter = Utils::EmailValidator::EmailValidatorAdapter.new
           return Presentation::Controllers::Signup::SignupController.new(email_validator_adapter)
         end
       end
---
+++ src/utils/email_validator/email_validator_adapter.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Utils
+  module EmailValidator
+    class EmailValidatorAdapter
+      def is_valid?(email)
+        regex = /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\z/
+        return !!(email =~ regex)
+      end
+    end
+  end
+end
