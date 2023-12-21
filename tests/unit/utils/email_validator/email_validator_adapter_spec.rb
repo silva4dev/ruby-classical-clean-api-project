d99d7b9c7630629a8aabb907f4605b51cabbba8e
silva4dev <web4dev.lucas@outlook.com>
2023-12-21 01:53:49 -0300

    test: ensure calls EmailValidatorAdapter to return true if validator returns true

--- tests/unit/utils/email_validator/email_validator_adapter_spec.rb
+++ tests/unit/utils/email_validator/email_validator_adapter_spec.rb
@@ -8,4 +8,10 @@ describe Utils::EmailValidator::EmailValidatorAdapter, type: :unit do
     allow(sut).to receive(:is_valid?).and_return(false)
     expect(sut.is_valid?("incorrect_email@mail.com")).to eq(false)
   end
+
+  it "Should return true if validator returns true" do
+    sut = Utils::EmailValidator::EmailValidatorAdapter.new
+    allow(sut).to receive(:is_valid?).and_return(true)
+    expect(sut.is_valid?("valid_email@mail.com")).to eq(true)
+  end
 end
