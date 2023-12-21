f4d70b2cf710b6a5eea4192c74b879c25cffac0b
silva4dev <web4dev.lucas@outlook.com>
2023-12-21 02:41:18 -0300

    refactor: move sut creation to a factory method

--- tests/unit/utils/email_validator/email_validator_adapter_spec.rb
+++ tests/unit/utils/email_validator/email_validator_adapter_spec.rb
@@ -3,20 +3,24 @@
 require_relative "../../../../src/utils/email_validator/email_validator_adapter"
 
 describe Utils::EmailValidator::EmailValidatorAdapter, type: :unit do
+  def make_sut
+    return Utils::EmailValidator::EmailValidatorAdapter.new
+  end
+
   it "Should return false if validator returns false" do
-    sut = Utils::EmailValidator::EmailValidatorAdapter.new
+    sut = make_sut()
     allow(sut).to receive(:is_valid?).and_return(false)
     expect(sut.is_valid?("incorrect_email@mail.com")).to eq(false)
   end
 
   it "Should return true if validator returns true" do
-    sut = Utils::EmailValidator::EmailValidatorAdapter.new
+    sut = make_sut()
     allow(sut).to receive(:is_valid?).and_return(true)
     expect(sut.is_valid?("valid_email@mail.com")).to eq(true)
   end
 
-   it "Should call validator with correct email" do
-    sut = Utils::EmailValidator::EmailValidatorAdapter.new
+  it "Should call validator with correct email" do
+    sut = make_sut()
     allow(sut).to receive(:is_valid?).and_return(true)
     sut.is_valid?("any_email@mail.com")
     expect(sut).to have_received(:is_valid?).with("any_email@mail.com")
