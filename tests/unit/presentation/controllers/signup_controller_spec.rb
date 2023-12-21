63312380a8390d46c0f72e581db94bce95c53377
silva4dev <web4dev.lucas@outlook.com>
2023-12-21 03:28:50 -0300

    test: ensure calls SignupController to return 400 if an invalid email is provided

--- tests/unit/presentation/controllers/signup_controller_spec.rb
+++ tests/unit/presentation/controllers/signup_controller_spec.rb
@@ -3,24 +3,20 @@
 require_relative "../../../../src/presentation/controllers/signup/signup_controller"
 require_relative "../../../../src/presentation/errors/missing_param_error"
 require_relative "../../../../src/presentation/errors/invalid_param_error"
+require_relative "../../../../src/utils/email_validator/email_validator_adapter"
 
 describe Presentation::Controllers::Signup::SignupController, type: :unit do
-  class EmailValidatorAdapterStub
-    def is_valid?(email)
-      return true
-    end
-  end
-
   def make_sut
-    email_validator_adapter_stub = EmailValidatorAdapterStub.new
-    return Presentation::Controllers::Signup::SignupController.new(email_validator_adapter_stub)
+    email_validator_adapter = Utils::EmailValidator::EmailValidatorAdapter.new
+    sut = Presentation::Controllers::Signup::SignupController.new(email_validator_adapter)
+    return { sut: sut, email_validator_adapter: email_validator_adapter }
   end
 
   it "Should return 400 if required fields are not passed in the HTTP request body" do
     http_request = {
       body: {},
     }
-    sut = make_sut()
+    sut = make_sut().fetch(:sut)
     http_response = sut.handle(http_request)
 
     expect(http_response[:status_code]).to eq(400)
@@ -33,13 +29,30 @@ describe Presentation::Controllers::Signup::SignupController, type: :unit do
         name: "any_name",
         email: "any_email@mail.com",
         password: "any_password",
-        password_confirmation: "any_password_confirmation",
+        password_confirmation: "incorrect_password",
       },
     }
-    sut = make_sut()
+    sut = make_sut().fetch(:sut)
     http_response = sut.handle(http_request)
 
     expect(http_response[:status_code]).to eq(400)
     expect(http_response[:body]).to eq(Presentation::Errors::InvalidParamError.new(:password_confirmation))
+  end
+
+  it "Should return 400 if an invalid email is provided" do
+    http_request = {
+      body: {
+        name: "any_name",
+        email: "incorrect_email@mail.com",
+        password: "any_password",
+        password_confirmation: "any_password",
+      },
+    }
+    sut, email_validator_adapter = make_sut().values_at(:sut, :email_validator_adapter)
+    allow(email_validator_adapter).to receive(:is_valid?).and_return(false)
+    http_response = sut.handle(http_request)
+
+    expect(http_response[:status_code]).to eq(400)
+    expect(http_response[:body]).to eq(Presentation::Errors::InvalidParamError.new(:email))
   end
 end
