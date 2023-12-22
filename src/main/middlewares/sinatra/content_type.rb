3a4681a9e0be14475f33eaf7151bcc6eba0d2c95
silva4dev <web4dev.lucas@outlook.com>
2023-12-21 23:24:16 -0300

    chore: ensure add handles content-type validation, returns a 415 error if content-type is not application/json

--- src/main/middlewares/sinatra/content_type.rb
+++ src/main/middlewares/sinatra/content_type.rb
@@ -9,7 +9,16 @@ module Main
         end
 
         def call(env)
+          request = ::Rack::Request.new(env)
           status, headers, body = @app.call(env)
+          if request.content_type != "application/json"
+            response_body = {
+              status_code: 415,
+              body: "Unsupported Media Type. Use Content-Type: application/json",
+            }.to_json
+            response = ::Rack::Response.new(response_body, 415, { "Content-Type" => "application/json" })
+            return response.finish
+          end
           headers["Content-Type"] = "application/json"
           [status, headers, body]
         end
