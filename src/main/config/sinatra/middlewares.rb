0b0bc554a3cb87bc44c4012f28aa1e1916e55295
silva4dev <web4dev.lucas@outlook.com>
2023-12-21 23:25:16 -0300

    chore: ensure add accept middleware

--- src/main/config/sinatra/middlewares.rb
+++ src/main/config/sinatra/middlewares.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "../../middlewares/sinatra/content_type"
+require_relative "../../middlewares/sinatra/accept"
 require_relative "../../middlewares/sinatra/cors"
 require "sinatra/base"
 
@@ -8,6 +9,7 @@ module Main
   module Config
     class Middlewares < Sinatra::Base
       use Main::Middlewares::Sinatra::ContentTypeMiddleware
+      use Main::Middlewares::Sinatra::AcceptMiddleware
       use Main::Middlewares::Sinatra::CorsMiddleware
     end
   end
