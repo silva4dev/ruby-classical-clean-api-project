3074f60baa9ccd36c36168b6e59e8c9f16780fcc
silva4dev <web4dev.lucas@outlook.com>
2023-12-21 23:25:01 -0300

    feat: ensure add accept middleware

---
+++ src/main/middlewares/sinatra/accept.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Main
+  module Middlewares
+    module Sinatra
+      class AcceptMiddleware
+        def initialize(app)
+          @app = app
+        end
+
+        def call(env)
+          request = ::Rack::Request.new(env)
+          status, headers, body = @app.call(env)
+          if request.env["HTTP_ACCEPT"] != "application/json"
+            response_body = {
+              status_code: 406,
+              body: "Not Acceptable. Use Accept: application/json",
+            }.to_json
+            response = ::Rack::Response.new(response_body, 406, { "Content-Type" => "application/json" })
+            return response.finish
+          end
+          [status, headers, body]
+        end
+      end
+    end
+  end
+end
