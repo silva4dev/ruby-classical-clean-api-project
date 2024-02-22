1a220e7ef2e79557b3b9b6db96a2a7d8dd19ea45
silva4dev <web4dev.lucas@outlook.com>
2024-02-22 15:55:07 -0300

    feat: add prefix to routes

--- src/main/config/sinatra/routes.rb
+++ src/main/config/sinatra/routes.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
 
 require 'sinatra/base'
+require 'sinatra/namespace'
 require_relative '../../routes/sinatra/signup_routes'
+require_relative '../../config/sinatra/middlewares'
 
 module Main
   module Config
     module SinatraRoutes
-      class Routes < Sinatra::Base
-        use Main::Routes::Sinatra::SignupRoutes
+      class RoutesSetup < Sinatra::Base
+        register Sinatra::Namespace
+        use Main::Config::SinatraMiddlewares::MiddlewaresSetup
+
+        namespace '/api' do
+          register Main::Routes::Sinatra::SignupRoutes
+        end
       end
     end
   end
--- src/main/routes/sinatra/signup_routes.rb
+++ src/main/routes/sinatra/signup_routes.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
-require_relative '../../../presentation/controllers/signup/signup_controller'
-require_relative '../../config/sinatra/middlewares'
 require_relative '../../adapters/sinatra/route_adapter'
 require_relative '../../factories/sinatra/signup'
 
 module Main
   module Routes
     module Sinatra
-      class SignupRoutes < Main::Config::SinatraMiddlewares::Middlewares
-        post '/signup' do
-          return Adapters::Sinatra::RouteAdapter.adapt(
-            Factories::Sinatra::SignupFactory.create)
-            .call(request, response)
-            .body
+      module SignupRoutes
+        def self.registered(app)
+          app.post '/signup' do
+            return Adapters::Sinatra::RouteAdapter.adapt(
+              Factories::Sinatra::SignupFactory.create)
+              .call(request, response)
+              .body
+          end
         end
       end
     end
