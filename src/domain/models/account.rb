1e6d094d32f3af8aa9eaf8bc5af754e62e447e94
silva4dev <web4dev.lucas@outlook.com>
2024-02-25 23:52:56 -0300

    feat: add validate in account model

--- src/domain/models/account.rb
+++ src/domain/models/account.rb
@@ -1,11 +1,28 @@
 # frozen_string_literal: true
 
+require 'securerandom'
+require_relative '../errors/invalid_type_error'
+require_relative '../errors/missing_value_error'
+
 class AccountModel
   attr_reader :id, :name, :email, :password
 
   def initialize(name, email, password)
+    @id = SecureRandom.uuid
     @name = name
     @email = email
     @password = password
+    validate
+  end
+
+  private
+
+  def validate
+    raise InvalidTypeError.new('Name', 'String') unless name.is_a?(String)
+    raise InvalidTypeError.new('Email', 'String') unless email.is_a?(String)
+    raise InvalidTypeError.new('Password', 'String') unless password.is_a?(String)
+    raise MissingValueError, 'Name' if name.empty?
+    raise MissingValueError, 'Email' if email.empty?
+    raise MissingValueError, 'Password' if password.empty?
   end
 end
